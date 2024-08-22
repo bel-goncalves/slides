91b947e96f9ac14d048442b96528386a1413b46f
bel-goncalves <igl@cesar.school>
2024-08-22 09:48:59 -0300

    video element

--- dist/Slide.js
+++ dist/Slide.js
@@ -14,7 +14,9 @@ export default class Slide {
         this.slides = slides;
         this.controls = controls;
         this.time = time;
-        this.index = 0;
+        this.index = localStorage.getItem("activeSlide")
+            ? Number(localStorage.getItem("activeSlide"))
+            : 0;
         this.slide = this.slides[this.index];
         this.timeout = null;
         this.paused = false;
@@ -26,10 +28,20 @@ export default class Slide {
         this.slide = this.slides[this.index];
         this.slides.forEach((slide) => this.hide(slide));
         this.slide.classList.add("active");
-        this.auto(this.time);
+        if (this.slide instanceof HTMLVideoElement) {
+            this.autoVideo(this.slide);
+        }
+        else {
+            this.auto(this.time);
+        }
+        localStorage.setItem("activeSlide", String(this.index));
     }
     hide(element) {
         element.classList.remove("active");
+        if (element instanceof HTMLVideoElement) {
+            element.currentTime = 0;
+            element.pause();
+        }
     }
     init() {
         this.addControls();
@@ -63,10 +75,24 @@ export default class Slide {
         this.timeout?.clear();
         this.timeout = new Timeout(() => this.next(), time);
     }
+    autoVideo(video) {
+        video.muted = true;
+        video.play();
+        let firstPlay = true;
+        video.addEventListener("playing", () => {
+            if (firstPlay)
+                this.auto(video.duration * 1000);
+            firstPlay = false;
+        });
+    }
     pause() {
         console.log("pausou");
         this.pausedTimeout = new Timeout(() => {
+            this.timeout?.pause();
             this.paused = true;
+            if (this.slide instanceof HTMLVideoElement) {
+                this.slide.pause();
+            }
         }, 300);
     }
     continue() {
@@ -75,6 +101,9 @@ export default class Slide {
         if (this.paused) {
             this.paused = false;
             this.timeout?.continue();
+            if (this.slide instanceof HTMLVideoElement) {
+                this.slide.play();
+            }
         }
     }
 }
--- src/Slide.ts
+++ src/Slide.ts
@@ -22,7 +22,9 @@ export default class Slide {
     this.controls = controls;
     this.time = time;
 
-    this.index = 0;
+    this.index = localStorage.getItem("activeSlide")
+      ? Number(localStorage.getItem("activeSlide"))
+      : 0;
     this.slide = this.slides[this.index];
     this.timeout = null;
     this.paused = false;
@@ -38,11 +40,21 @@ export default class Slide {
     this.slides.forEach((slide) => this.hide(slide));
     this.slide.classList.add("active");
 
-    this.auto(this.time);
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autoVideo(this.slide);
+    } else {
+      this.auto(this.time);
+    }
+
+    localStorage.setItem("activeSlide", String(this.index));
   }
 
   hide(element: Element) {
     element.classList.remove("active");
+    if (element instanceof HTMLVideoElement) {
+      element.currentTime = 0;
+      element.pause();
+    }
   }
 
   private init() {
@@ -83,10 +95,25 @@ export default class Slide {
     this.timeout = new Timeout(() => this.next(), time);
   }
 
+  autoVideo(video: HTMLVideoElement) {
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener("playing", () => {
+      if (firstPlay) this.auto(video.duration * 1000); //miliseg
+      firstPlay = false;
+    });
+  }
+
   pause() {
     console.log("pausou");
     this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
       this.paused = true;
+
+      if (this.slide instanceof HTMLVideoElement) {
+        this.slide.pause();
+      }
     }, 300);
   }
 
@@ -96,6 +123,10 @@ export default class Slide {
     if (this.paused) {
       this.paused = false;
       this.timeout?.continue();
+
+      if (this.slide instanceof HTMLVideoElement) {
+        this.slide.play();
+      }
     }
   }
 }
